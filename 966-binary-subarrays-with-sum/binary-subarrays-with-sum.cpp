c87ba90c8356e3c9f707c2b78c38e203c0946ddd
Rakesh Kumar <115536876+thisisrakeshbarik@users.noreply.github.com>
2025-09-01 15:46:15 +0530

    Time: 23 ms (20.16%) | Memory: 38.8 MB (13.24%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-int cntGoal(vector<int>& nums, int goal){
-      int n = nums.size();
-      int l = 0;
-      int r = 0;
-      int cnt = 0;
-      long long sum = 0;
-      while(r<n){
-        sum+=nums[r];
-        while(l<=r && sum>goal){
-            sum-=nums[l];
-            l++;
-        }
-        cnt += (r - l + 1);
-        r++;
-      }
-      return cnt;
-}
-
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int g1 = cntGoal(nums,goal);
-        int g2 = cntGoal(nums,goal-1);
-        return g1-g2;  
+        int totalCount = 0;
+        int currentSum = 0;
+        // {prefix: number of occurrence}
+        unordered_map<int, int> freq; // To store the frequency of prefix sums
+
+        for (int num : nums) {
+            currentSum += num;
+            if (currentSum == goal){
+                totalCount++;
+            }
+
+            // Check if there is any prefix sum that can be subtracted from the current sum to get the desired goal
+            if (freq.find(currentSum - goal) != freq.end()){
+                totalCount += freq[currentSum - goal];
+            }
+
+            freq[currentSum]++;
+        }
+
+        return totalCount;
     }
 };
