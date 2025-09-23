22488cd2c1eb1f5ac0d5c10b0931edbfbdd74563
Rakesh Kumar <115536876+thisisrakeshbarik@users.noreply.github.com>
2025-09-23 15:18:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (48.40%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i = 0, j = 0;
+        int n = version1.size(), m = version2.size();
+        
+        while (i < n || j < m) {
+            long num1 = 0, num2 = 0;
+            
+            // extract number from version1
+            while (i < n && version1[i] != '.') {
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
+            }
+            // extract number from version2
+            while (j < m && version2[j] != '.') {
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
+            }
+            
+            if (num1 > num2) return 1;
+            if (num1 < num2) return -1;
+            
+            // skip the '.'
+            i++;
+            j++;
+        }
+        
+        return 0;
+    }
+};
